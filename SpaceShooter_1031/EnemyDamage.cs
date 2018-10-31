f3d1018ec615c2654a1f60478b1dfec1e43913ae
greenleaf-yay <greenleaf.yay@gmail.com>
2018-10-31 16:12:11 +0900

    Drag, Drop, EAI, EDamage, GM

---
+++ SpaceShooter_1031/EnemyDamage.cs
@@ -0,0 +1,97 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class EnemyDamage : MonoBehaviour
+{
+    private const string bulletTag = "BULLET";
+
+    public float hp = 100.0f;
+
+    private float initHp = 100.0f;
+    private GameObject bloodEffect;
+    private Canvas uiCanvas;
+    private Image hpBarImage;
+
+    public GameObject hpBarPrefab;
+    public Vector3 hpBarOffset = new Vector3(0.0f, 2.2f, 0.0f);
+
+    private GameObject hpBar;
+
+    void Start()
+    {
+        bloodEffect = Resources.Load<GameObject>
+            ("BulletImpactFleshBigEffect");
+
+        //hpBar = Instantiate<GameObject>
+        //    (hpBarPrefab, uiCanvas.transform);
+
+        SetHpBar();
+    }
+
+    void SetHpBar()
+    {
+        uiCanvas = GameObject.Find("UI Canvas")
+            .GetComponent<Canvas>();
+
+        hpBar = Instantiate<GameObject>
+            (hpBarPrefab, uiCanvas.transform);
+
+        //GameObject hpBar = Instantiate<GameObject>
+        //    (hpBarPrefab, uiCanvas.transform);
+
+        //hpBarImage = hpBar.GetComponentsInChildren<Image>()[1];
+        //var _hpBar = hpBar.GetComponent<EnemyHpBar>();
+
+        //_hpBar.targetTr = this.gameObject.transform;
+        //_hpBar.offset = hpBarOffset;
+
+        ShowHpBar();
+    }
+
+    public void ShowHpBar()
+    {
+        hpBarImage = hpBar.GetComponentsInChildren<Image>()[1];
+        hpBarImage.GetComponentsInParent<Image>()[1]
+                    .color = Color.black;
+        hpBarImage.fillAmount = hp / initHp;
+        var _hpBar = hpBar.GetComponent<EnemyHpBar>();
+
+        _hpBar.targetTr = this.gameObject.transform;
+        _hpBar.offset = hpBarOffset;
+    }
+    
+    private void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.tag == bulletTag)
+        {
+            ShowBloodEffect(coll);
+            //Destroy(coll.gameObject);
+            coll.gameObject.SetActive(false);
+            hp -= coll.gameObject.GetComponent<BulletCtrl>().
+                damage;
+            hpBarImage.fillAmount = hp / initHp;
+            if (hp <= 0.0f)
+            {
+                GetComponent<EnemyAI>()._state
+                    = EnemyAI.State.DIE;
+                hpBarImage.GetComponentsInParent<Image>()[1]
+                    .color = Color.clear;
+                GameManager.instance.AddKillCount();
+                GetComponent<CapsuleCollider>().enabled = false;
+            }
+        }
+    }
+
+    void ShowBloodEffect(Collision coll)
+    {
+        Vector3 pos = coll.contacts[0].point;
+        Vector3 _normal = coll.contacts[0].normal;
+        Quaternion rot = Quaternion.FromToRotation
+            (-Vector3.forward, _normal);
+        GameObject blood = Instantiate<GameObject>
+            (bloodEffect, pos, rot);
+        Destroy(blood, 1.0f);
+    }
+}
---
+++ SpaceShooter_1031/GameManager.cs
@@ -0,0 +1,171 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    [Header("Enemy Create Info")]
+    public Transform[] points;
+    public GameObject enemy;
+    public float createTime = 2.0f;
+    public int maxEnemy = 10;
+    public bool isGameOver = false;
+
+    //public bool show = false;
+
+    //싱글턴을 위한 객체 선언
+    public static GameManager instance = null;
+
+    [Header("Object Pool")]
+    public GameObject bulletPrefab;
+    public int maxBulletPool = 10;
+    public List<GameObject> bulletPool = new List<GameObject>();
+    public GameObject enemyPrefab;
+    public int maxEnemyPool = 10;
+    public List<GameObject> enemyPool = new List<GameObject>();
+    public CanvasGroup inventoryCG;
+
+    [HideInInspector] public int killCount;
+    public Text killCountTxt;
+
+    private bool isPause = false;    
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this.gameObject); //다른 씬에서도 삭제안됨
+        LoadGameData();
+        CreatePool();
+    }
+
+    void LoadGameData()
+    {
+        killCount = PlayerPrefs.GetInt("KILL_COUNT", 0);
+        killCountTxt.text = "KILL " + killCount.ToString("0000");
+    }
+
+    void Start()
+    {
+        OnInventoryOpen(false);
+        points = GameObject.Find("SpawnPointGroup")
+            .GetComponentsInChildren<Transform>();
+        if (points.Length > 0)
+        {
+            StartCoroutine(this.CreateEnemy());
+        }
+    }
+
+    public void OnInventoryOpen(bool isOpen)
+    {
+        inventoryCG.alpha = (isOpen) ? 1.0f : 0.0f;
+        inventoryCG.interactable = isOpen;
+        inventoryCG.blocksRaycasts = isOpen;
+    }
+
+    IEnumerator CreateEnemy()
+    {
+        while (!isGameOver)
+        {
+            int enemyCount = (int)GameObject
+                .FindGameObjectsWithTag("ENEMY").Length;
+            if (enemyCount < maxEnemy)
+            {
+                yield return new WaitForSeconds(createTime);
+                //int idx = Random.Range(1, points.Length);
+                //Instantiate(enemy, points[idx].position,
+                //    points[idx].rotation);
+                var _enemy = GetEnemy();
+                if (_enemy != null)
+                {
+                    int idx = Random.Range(1, points.Length);
+                    _enemy.transform.position = points[idx].position;
+                    _enemy.transform.rotation = points[idx].rotation;
+                    _enemy.SetActive(true);
+
+                    //_enemy.SendMessage("ShowHpBar"
+                    //    , SendMessageOptions.DontRequireReceiver);
+
+                    //show = true;
+                }
+            }
+            else yield return null;            
+        }
+    }
+
+    public GameObject GetBullet()
+    {
+        for (int i = 0; i < bulletPool.Count; i++)
+        {
+            if (bulletPool[i].activeSelf == false)
+            {
+                return bulletPool[i];
+            }
+        }
+        return null;
+    }
+
+    public GameObject GetEnemy()
+    {
+        for (int i = 0; i < enemyPool.Count; i++)
+        {
+            if (enemyPool[i].activeSelf == false)
+            {
+                return enemyPool[i];
+            }
+        }
+        return null;
+    }
+
+    public void CreatePool()
+    {
+        GameObject bulletPools = new GameObject("BulletPools");
+        for (int i = 0; i < maxBulletPool; i++)
+        {
+            var obj = Instantiate<GameObject>(bulletPrefab
+                , bulletPools.transform);
+            obj.name = "Bullet_" + i.ToString("00");
+            obj.SetActive(false);
+            bulletPool.Add(obj);
+        }
+        GameObject enemyPools = new GameObject("EnemyPools");
+        for (int i = 0; i < maxEnemyPool; i++)
+        {
+            var obj = Instantiate<GameObject>(enemyPrefab
+                , enemyPools.transform);
+            obj.name = "Enemy_" + i.ToString("00");
+            obj.SetActive(false);
+            enemyPool.Add(obj);
+        }
+    }
+
+    public void OnPauseClick()
+    {
+        isPause = !isPause;
+        Time.timeScale = (isPause) ? 0.0f : 1.0f;
+        var playerObj = GameObject.FindGameObjectWithTag("Player");
+        var scripts = playerObj.GetComponents<MonoBehaviour>();
+        foreach (var item in scripts)
+        {
+            item.enabled = !isPause;
+        }
+
+        var canvasGroup = GameObject.Find("Panel-Weapon")
+            .GetComponent<CanvasGroup>();
+        canvasGroup.blocksRaycasts = !isPause;
+    }
+    
+    public void AddKillCount()
+    {
+        ++killCount;
+        killCountTxt.text = "KILL " + killCount.ToString("0000");
+        PlayerPrefs.SetInt("KILL_COUNT", killCount);
+    }
+}
