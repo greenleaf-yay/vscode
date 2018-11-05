db8d21152ef7ac5db0604385579c0d48f19fd1fb
greenleaf-yay <greenleaf.yay@gmail.com>
2018-11-05 14:17:39 +0900

    SS_181105

---
+++ SS_1105/FireCtrl.cs
@@ -0,0 +1,149 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+[System.Serializable]
+public struct PlayerSfx
+{
+    public AudioClip[] fire;
+    public AudioClip[] reload;
+}
+
+public class FireCtrl : MonoBehaviour
+{
+    public enum WeaponType
+    {
+        RIFLE = 0, SHOTGUN, FLAMETHROWER, ROCKET
+    }
+
+    public WeaponType currWeapon = WeaponType.RIFLE;
+    public GameObject bullet;
+    public Transform firePos;
+    public float fireRate = 0.1f;
+    public ParticleSystem cartridge;    
+    public PlayerSfx playerSfx;
+    public Image magazineImg;
+    public Text magazineText;
+    public int maxBullet = 10;
+    public int remainingBullet = 10;
+    //public float reloadTime = 2.0f;
+    
+    private ParticleSystem muzzleFlash;
+    private float nextFire = 0.0f;
+    private AudioSource _audio;
+    private Shake _shake;
+    private bool isReloading = false;
+    private bool isFire = false;
+    private int enemyLayer;
+    private int obstacleLayer;
+    private int layerMask;
+
+    public Sprite[] weaponIcons;
+    public Image weaponImage;
+
+    void Start()
+    {
+        muzzleFlash = firePos.
+            GetComponentInChildren<ParticleSystem>();
+        _audio = GetComponent<AudioSource>();
+        _shake = GameObject.Find("CameraRig")
+            .GetComponent<Shake>();
+        enemyLayer = LayerMask.NameToLayer("ENEMY");
+        obstacleLayer = LayerMask.NameToLayer("OBSTACLE");
+        layerMask = 1 << obstacleLayer | 1 << enemyLayer;
+    }
+
+    void Update()
+    {
+        Debug.DrawRay(firePos.position, firePos.forward * 20.0f
+            , Color.green);
+
+        if (EventSystem.current.IsPointerOverGameObject())return;
+
+        RaycastHit hit;
+        if (Physics.Raycast(firePos.position, firePos.forward
+            , out hit, 20.0f, layerMask))
+        {
+            isFire = (hit.collider.CompareTag("ENEMY"));
+        }
+        else isFire = false;
+
+        if (!isReloading && isFire)
+        {
+            if (Time.time >= nextFire)
+            {
+                --remainingBullet;
+                Fire();
+                if (remainingBullet == 0)
+                {
+                    StartCoroutine(Reloading());
+                }
+                nextFire = Time.time + fireRate;
+            }
+        }
+
+        if (!isReloading && Input.GetMouseButtonDown(0))
+        {
+            --remainingBullet;
+            Fire();
+            if (remainingBullet == 0)
+            {
+                StartCoroutine(Reloading());
+            }
+        }
+    }
+
+    void Fire()
+    {
+        StartCoroutine(_shake.ShakeCamera());
+        //GameObject _bullet = Instantiate(bullet,
+        //    firePos.position, firePos.rotation);
+        //Destroy(_bullet, 3.0f);
+        var _bullet = GameManager.instance.GetBullet();
+        if (_bullet != null)
+        {
+            _bullet.transform.position = firePos.position;
+            _bullet.transform.rotation = firePos.rotation;
+            _bullet.SetActive(true);
+        }
+        muzzleFlash.Play();
+        cartridge.Play();
+        FireSfx();
+        magazineImg.fillAmount = (float)remainingBullet / 
+            (float)maxBullet;
+        UpdateBulletText();
+    }
+
+    void FireSfx()
+    {
+        var _sfx = playerSfx.fire[(int)currWeapon];
+        _audio.PlayOneShot(_sfx);
+    }
+
+    IEnumerator Reloading()
+    {
+        isReloading = true;
+        _audio.PlayOneShot(playerSfx.reload[(int)currWeapon]
+            , 1.0f);
+        yield return new WaitForSeconds(playerSfx
+            .reload[(int)currWeapon].length + 0.3f);
+        isReloading = false;
+        magazineImg.fillAmount = 1.0f;
+        remainingBullet = maxBullet;
+        UpdateBulletText();
+    }
+
+    void UpdateBulletText()
+    {
+        magazineText.text = string.Format("<color=#ff0000>" +
+            "{0}</color>/{1}", remainingBullet, maxBullet);
+    }
+
+    public void OnChangeWeapon()
+    {
+        currWeapon = (WeaponType)((int)++currWeapon % 2);
+        weaponImage.sprite = weaponIcons[(int)currWeapon];
+    }
+}
---
+++ SS_1105/RemoveBullet.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RemoveBullet : MonoBehaviour
+{
+    public GameObject sparkEffect;
+
+    private void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.tag == "BULLET")
+        {
+            ShowEffect(coll);
+
+            //Destroy(coll.gameObject);
+            coll.gameObject.SetActive(false);
+        }
+    }
+
+    void ShowEffect(Collision coll)
+    {
+        ContactPoint contact = coll.contacts[0];
+        Quaternion rot = Quaternion.
+            FromToRotation(-Vector3.forward, contact.normal);
+        GameObject spark = Instantiate(sparkEffect,
+            contact.point, rot);
+        spark.transform.SetParent(this.transform);
+    }
+}
