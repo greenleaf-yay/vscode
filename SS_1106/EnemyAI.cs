5dc9630f51f61a0b0e49f9ec197f6fcd1c19e9f8
greenleaf-yay <greenleaf.yay@gmail.com>
2018-11-06 14:36:39 +0900

    SpaceShooter_181106

---
+++ SS_1106/EnemyAI.cs
@@ -0,0 +1,168 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    public enum State { PATROL, TRACE, ATTACK, DIE }
+    public State _state = State.PATROL;
+    public float attackDist = 5.0f;
+    public float traceDist = 10.0f;
+    public bool isDie = false;
+    //public bool showHpBar = false;
+
+    private Transform playerTr;
+    private Transform enemyTr;
+    private WaitForSeconds ws;
+    private MoveAgent mvAgent;
+    private Animator _animator;
+    private EnemyFire _enemyFire;
+    private EnemyDamage _enemyDamage;
+    private readonly int hashMove = Animator.
+        StringToHash("IsMove");
+    private readonly int hashSpeed = Animator.
+        StringToHash("Speed");
+    private readonly int hashDie = Animator.
+        StringToHash("Die");
+    private readonly int hashDieIdx = Animator.
+        StringToHash("DieIdx");
+    private readonly int hashOffset = Animator.
+        StringToHash("Offset");
+    private readonly int hashWalkSpeed = Animator.
+        StringToHash("WalkSpeed");
+    private readonly int hashPlayerDie = Animator.
+        StringToHash("PlayerDie");
+
+    private void Awake()
+    {
+        var player = GameObject.FindGameObjectWithTag("Player");
+        if (player != null)
+        {
+            playerTr = player.GetComponent<Transform>();
+        }
+        enemyTr = GetComponent<Transform>();
+        mvAgent = GetComponent<MoveAgent>();
+        _animator = GetComponent<Animator>();
+        _enemyFire = GetComponent<EnemyFire>();
+        _enemyDamage = GetComponent<EnemyDamage>();
+        ws = new WaitForSeconds(0.3f);
+        _animator.SetFloat(hashOffset, Random.Range(0.0f, 1.0f));
+        _animator.SetFloat(hashWalkSpeed, 
+            Random.Range(1.0f, 1.2f));
+    }
+
+    private void OnEnable() // 해당 오브젝트가 활성화 될때 실행
+    {
+        StartCoroutine(CheckState());
+        StartCoroutine(Action());
+        Damage.OnPlayerDie += this.OnPlayerDie;
+
+        this.gameObject.tag = "ENEMY";
+        isDie = false;
+        _enemyFire.isFire = false;
+        //mvAgent.Stop();
+        GetComponent<CapsuleCollider>().enabled = true;
+    }
+
+    private void OnDisable() // 해당 오브젝트가 비활성화 될때 실행
+    {
+        Damage.OnPlayerDie -= this.OnPlayerDie;
+    }
+
+    IEnumerator CheckState()
+    {
+        while(!isDie)
+        {
+            if (_state == State.DIE) yield break;
+            float dist = Vector3.Distance(playerTr.position,
+                enemyTr.position);
+            if (dist <= attackDist)
+            {
+                _state = State.ATTACK;
+            }
+            else if (dist <= traceDist)
+            {
+                _state = State.TRACE;
+            }
+            else
+            {
+                _state = State.PATROL;
+            }
+            yield return ws;
+        }
+    }
+
+    IEnumerator Action()
+    {
+        while(!isDie)
+        {
+            yield return ws;
+            switch (_state)
+            {
+                case State.PATROL:
+                    _enemyFire.isFire = false;
+                    mvAgent.Patrolling = true;
+                    _animator.SetBool(hashMove, true);
+                    break;
+                case State.TRACE:
+                    _enemyFire.isFire = false;
+                    mvAgent.TraceTarget = playerTr.position;
+                    _animator.SetBool(hashMove, true);
+                    break;
+                case State.ATTACK:
+                    _enemyFire.isFire = true;
+                    mvAgent.Stop();
+                    _animator.SetBool(hashMove, false);
+                    break;
+                case State.DIE:
+
+                    this.gameObject.tag = "Untagged";
+                    //StopAllCoroutines();
+                    isDie = true;
+                    _enemyFire.isFire = false;
+                    mvAgent.Stop();
+                    _animator.SetInteger(hashDieIdx,
+                        Random.Range(0, 3));
+                    _animator.SetTrigger(hashDie);
+                    GetComponent<CapsuleCollider>().enabled = false;
+                    StartCoroutine(PushEnemyPool());
+
+                    break;
+            }
+        }
+    }
+
+    IEnumerator PushEnemyPool()
+    {
+        yield return new WaitForSeconds(5.0f);
+        isDie = false;
+        _enemyDamage.hp = 100.0f;
+
+        _enemyDamage.ShowHpBar();
+
+        gameObject.tag = "ENEMY";
+        _state = State.PATROL;
+        GetComponent<CapsuleCollider>().enabled = true;
+        gameObject.SetActive(false);
+    }
+
+    void Update()
+    {
+        _animator.SetFloat(hashSpeed, mvAgent.Speed);
+
+        //if (GameManager.instance.show)
+        //{
+        //    _enemyDamage.ShowHpBar();
+        //    GameManager.instance.show = false;
+        //}
+    }
+
+    public void OnPlayerDie()
+    {
+        mvAgent.Stop();
+        _enemyFire.isFire = false;
+        StopAllCoroutines();
+
+        _animator.SetTrigger(hashPlayerDie);
+    }
+}
---
+++ SS_1106/FollowCam.cs
@@ -0,0 +1,87 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCam : MonoBehaviour
+{
+    public Transform target;
+    public float moveDamping = 15.0f;
+    public float rotateDamping = 10.0f;
+    public float distance = 4.0f;
+    public float height = 3.0f;
+    public float targetOffset = 2.0f;
+
+    [Header("Wall Object Setting")]
+    public float heightAboveWall = 10.0f;
+    public float colliderRadius = 1.8f;
+    public float overDamping = 5.0f;
+
+    private float originHeight;
+
+    [Header("Etc Obstacle Setting")]
+    public float heightAboveObstacle = 12.0f;
+    public float castOffset = 1.0f;
+
+    private Transform tr;
+
+    void Start()
+    {
+        tr = GetComponent<Transform>();
+        originHeight = height;
+    }
+
+    private void Update()
+    {
+        if (Physics.CheckSphere(tr.position, colliderRadius))
+        {
+            height = Mathf.Lerp(height, heightAboveWall
+                , Time.deltaTime * overDamping);
+        }
+        else
+        {
+            height = Mathf.Lerp(height, originHeight
+                , Time.deltaTime * overDamping);
+        }
+
+        Vector3 castTarget = target.position + (target.up
+            * castOffset);
+        Vector3 castDir = (castTarget - tr.position).normalized;
+        RaycastHit hit;
+        if (Physics.Raycast(tr.position, castDir, out hit
+            , Mathf.Infinity))
+        {
+            if (!hit.collider.CompareTag("Player"))
+            {
+                height = Mathf.Lerp(height, heightAboveObstacle
+                , Time.deltaTime * overDamping);
+            }
+            else
+            {
+                height = Mathf.Lerp(height, originHeight
+                    , Time.deltaTime * overDamping);
+            }
+        }
+    }
+
+    void LateUpdate()
+    {
+        var camPos = target.position -
+            (target.forward * distance) + (target.up * height);
+        tr.position = Vector3.Slerp(tr.position, camPos
+            , Time.deltaTime * moveDamping);
+        tr.rotation = Quaternion.Slerp(tr.rotation
+            , target.rotation, Time.deltaTime * rotateDamping);
+        tr.LookAt(target.position + (target.up * targetOffset));
+    }
+
+    private void OnDrawGizmos()
+    {
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(target.position +
+            (target.up * targetOffset), 0.1f);
+        Gizmos.DrawLine(target.position +
+            (target.up * targetOffset), transform.position);
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, colliderRadius);
+    }
+}
