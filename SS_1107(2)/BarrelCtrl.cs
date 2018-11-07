3f66a32ea388de51b996ce9706079279b0349571
greenleaf-yay <greenleaf.yay@gmail.com>
2018-11-07 13:54:25 +0900

    SpaceShooter_181107(2)

---
+++ SS_1107(2)/BarrelCtrl.cs
@@ -0,0 +1,110 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BarrelCtrl : MonoBehaviour
+{
+    public GameObject expEffect;
+    public GameObject fireEffect;
+    public GameObject smokeEffect;
+    public Mesh[] meshes;
+    public Texture[] textures;
+    public float expRadius = 10.0f;
+    public AudioClip expSfx;
+
+    private int hitCount = 0;
+    private Rigidbody rb;
+    private MeshFilter _meshFilter;
+    private MeshRenderer _renderer;
+    private bool isFire = false;
+    private AudioSource _audio;
+    private Shake _shake;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        _meshFilter = GetComponent<MeshFilter>();
+        _renderer = GetComponent<MeshRenderer>();
+        _renderer.material.mainTexture
+            = textures[Random.Range(0, textures.Length - 1)];
+        fireEffect.SetActive(false);
+        smokeEffect.SetActive(false);
+        _audio = GetComponent<AudioSource>();
+        _shake = GameObject.Find("CameraRig")
+            .GetComponent<Shake>();
+    }
+
+    void OnDamage(object[] _params) //드럼통에 물리력을 주기 위해서 입사각을 구함
+    {
+        Vector3 hitPos = (Vector3)_params[0];
+        Vector3 firePos = (Vector3)_params[3];
+        Vector3 incomeVector = (hitPos - firePos).normalized; //입사각 //normalized 정규화(1)
+        rb.AddForceAtPosition(incomeVector * 1000.0f, hitPos); //AddForceAtPosition = hit된 위치에 힘을 가한다
+        if (++hitCount == 3)
+        {
+            ExpBarrel();
+        }
+    }
+    private void OnCollisionEnter(Collision coll)
+    {
+        if (coll.collider.CompareTag("BULLET"))
+        {
+            if (++hitCount == 3)
+            {
+
+                ExpBarrel();
+            }
+        }
+    }
+
+    void ExpBarrel()
+    {
+        _audio.PlayOneShot(expSfx);
+        GameObject effect = Instantiate(expEffect,
+            transform.position, Quaternion.identity);
+        _renderer.material.mainTexture
+            = textures[textures.Length - 1];
+        OnFire();
+        IndirectDamage(transform.position);
+        Destroy(effect, 3.8f);
+        int idx = Random.Range(0, meshes.Length);
+        _meshFilter.sharedMesh = meshes[idx];
+        //Destroy(this.gameObject, 10.0f);
+        StartCoroutine(_shake.ShakeCamera(0.1f, 0.2f, 0.5f));
+    }
+
+    void IndirectDamage(Vector3 pos)
+    {
+        Collider[] colls = Physics.OverlapSphere(pos, expRadius,
+            3 << 8);
+        foreach (var item in colls)
+        {
+            if (item.gameObject.tag != "Player")
+            {
+                var _rb = item.GetComponent<Rigidbody>();
+                if (item.GetComponent<BarrelCtrl>().isFire == false)
+                {
+                    item.GetComponent<BarrelCtrl>().isFire = true;
+                    StartCoroutine(item.GetComponent<BarrelCtrl>()
+                    .DelayExp());
+                }
+                _rb.mass = 1.0f;
+                _rb.AddExplosionForce(1200.0f, pos, expRadius
+                    , 1000.0f);
+            }
+        }
+    }
+
+    IEnumerator DelayExp()
+    {
+        this.gameObject.GetComponent<BarrelCtrl>().OnFire();
+        yield return new WaitForSeconds(Random.Range(5.0f, 10.0f));
+        this.ExpBarrel();
+    }
+
+    void OnFire()
+    {
+        fireEffect.SetActive(true);
+        smokeEffect.SetActive(true);
+    }
+}
---
+++ SS_1107(2)/LaserBeam.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LaserBeam : MonoBehaviour
+{
+    private Transform tr;
+    private LineRenderer line;
+    private RaycastHit hit;
+    private float fireRate;
+    private float nextFire = 0.0f;
+
+    private void Start()
+    {
+        tr = GetComponent<Transform>();
+        line = GetComponent<LineRenderer>();
+        line.useWorldSpace = false;
+        line.enabled = false; //처음엔 안보이게 비활성화
+        line.startWidth = 0.1f; //레이저빔 두께 시작폭 10cm
+        line.endWidth = 0.01f; //레이저빔 두께 끝 폭 1cm
+        fireRate = GetComponentInParent<FireCtrl>().fireRate; //GetComponentInParent 상위로 올라가면서 <FireCtrl>를 찾음
+    }
+
+    private void Update()
+    {
+        Ray ray = new Ray(tr.position, tr.forward); //ray는 앞쪽방향 위치
+        if (Input.GetMouseButton(0))
+        {
+            if (Time.time >= nextFire)
+            {
+                line.SetPosition(0, tr.InverseTransformPoint(ray.origin));
+                if (Physics.Raycast(ray, out hit, 100.0f))
+                {
+                    line.SetPosition(1, tr.InverseTransformPoint(hit.point));
+                }
+                else line.SetPosition(1, tr.InverseTransformPoint(ray.GetPoint(100.0f))); //ray = 무한히 뻗는 ray에 GetPoint 값을가져온다 100m까지
+                StartCoroutine(this.ShowLaserBeam()); //코르틴함수 적용
+                nextFire = Time.time + fireRate;
+            }
+        }
+    }
+    IEnumerator ShowLaserBeam()
+    {
+        line.enabled = true;
+        yield return new WaitForSeconds(Random.Range(0.01f, 0.2f));
+        line.enabled = false;
+    }
+}
